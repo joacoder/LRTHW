a8c839e87d5bbfc8c5e6d84b3c0c15edd98c0725
joacoder <joaquin.coronel342@gmail.com>
2017-02-09 10:02:59 -0500

    Add files via upload

---
+++ ex6.rb
@@ -0,0 +1,28 @@
+#this is a variable
+types_of_people = 10
+#This is using the variable in a string
+x = "There are #{types_of_people} types of peoples."
+#Another variable
+binary = "binary"
+#Yet another variable
+do_not = "don't"
+#punchline using the two previous variables
+y = "Those who know #{binary} and those who #{do_not}."
+#These next two lines bring the joke together
+puts x
+puts y
+#This prints the first part of the joke again
+puts "I said: #{x}."
+#This prints the punchline again
+puts "I also said: '#{y}'."
+#This is sarcasm, just cheeky sarcasm.
+hilarious = false
+#This attempts to boost self esteem
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+#This the self awareness part
+puts joke_evaluation
+#This is just cheeky
+w =  "This is the left side of..."
+e =  "a string with a right side."
+
+puts w + e
